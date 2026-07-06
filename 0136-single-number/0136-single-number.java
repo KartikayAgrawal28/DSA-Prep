9ccb1589a015de4becbac8647be1e984064dc80e
Kartikay_Agrawal <kartikayagrawal28@gmail.com>
2026-07-06 12:30:31 +0530

    Time: 1 ms (99.97%), Space: 47 MB (27.92%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,22 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        int count = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-
-            int count = 0;
-
-            for (int j = 0; j < nums.length; j++) {
-
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-
-            if (count == 1) {
-                return nums[i];
-            }
+        // by using xor property we can solve this question
+        //1: x^0=x
+        //2: x^x=0
+        // 3: a^b^c = a^(b^c) = (a^b)^c = (a^c)^b
+        for(int i:nums){
+            count^=i;
         }
 
-        return -1;
+        return count;
+        
     }
 }
