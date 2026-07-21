4a57b999029494b099379842881c04034fed3f02
Kartikay_Agrawal <kartikayagrawal28@gmail.com>
2026-07-22 00:53:24 +0530

    Time: 2 ms (99.98%), Space: 48.3 MB (72.03%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,34 +1,13 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int count=0, prod=1;
+        int count=0,prod=1,l=0;
         int n = nums.length;
 
-        // check base case 
+        // check base case  
         if(k<=1) return 0;  
-        /* 
-
-        Aproach 1 --> BRUTE FORCE O(2N)
-
-        for(int i=0;i<n;i++){
-            
-            prod=1;
-            for(int j=i;j>=0;j--){
-                prod *= nums[j];
-
-                if(prod<k){
-                    count++;
-                }
-                else{
-                    break;
-                }
-            }
-        }*/
-
-
+        
         // APPROACH 2 ----> OPTIMISED
-
-        int l=0;
-
+        
         for(int r=0 ; r<n ; r++){
             prod *= nums[r];
 
