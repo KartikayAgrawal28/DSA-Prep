79435667613ee816df8786f7c8d0911e3c1abcb0
Kartikay_Agrawal <kartikayagrawal28@gmail.com>
2026-07-28 00:34:41 +0530

    Time: 33 ms (91.59%), Space: 98.7 MB (74.51%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,42 +1,32 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        //use hashmap to store frequency
-        HashMap<Integer,Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
 
-        long ans=0, sum=0;
+        long sum=0,ans= 0;
         int left=0;
 
         for(int right=0;right<nums.length;right++){
 
-            //expansion
-            sum+= nums[right];
-            map.put(nums[right] , map.getOrDefault(nums[right],0)+1);
-
-            //keep window exact k
-            if((right-left+1)>k){
+            //shrinking if already present in set
+            while(set.contains(nums[right])){
+                set.remove(nums[left]);
                 sum-=nums[left];
-
-                //remove the repeated number freq from map
-                map.put(nums[left] , map.get(nums[left])-1);
-
-                //remove if freq=0
-                if(map.get(nums[left])==0){
-                    map.remove(nums[left]);
-                }
                 left++;
             }
 
-            //checks for valid window
-            // right-left+1=subaray size and map.size()=distinct character in map 
-            if(right-left+1 == k && map.size()==k){ 
+            //expansion
+            set.add(nums[right]);
+            sum += nums[right];
+
+            
+            if(right-left+1==k){
                 ans = Math.max(ans,sum);
+
+                set.remove(nums[left]);
+                sum-=nums[left];
+                left++;
             }
         }
         return ans;
-
-        
-
-
-        
     }
 }
